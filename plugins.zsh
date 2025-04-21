5ebd8e1c388dd8206e1c545553229a70d985089e
saulo217 <78312710+Saulo217@users.noreply.github.com>
2025-04-21 16:31:49 -0300

    feat: added plugins list

---
+++ plugins.zsh
@@ -0,0 +1,10 @@
+#!/bin/env zsh
+
+# zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# zsh-completions
+fpath=("~/.zsh/zsh-completions/src" $fpath)
+
+# zsh-syntax-highlighting
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
